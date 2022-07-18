68086eedc306f2f87b9d20d89712e42e675e948b
jnni <jonathan.moraes@gft.com>
2022-07-18 10:36:46 -0300

    projeto MVC

---
+++ src/main/java/com/mballem/curso/security/DemoSecurityApplication.java
@@ -0,0 +1,19 @@
+package com.mballem.curso.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.mballem.curso.security.service.EmailService;
+
+@SpringBootApplication
+public class DemoSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSecurityApplication.class, args);
+	}
+
+	@Autowired
+	EmailService service;	
+
+}
